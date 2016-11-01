fce6ebaa0cb4d936c87280ac9681096c46df8aca
Nlipski <13ml76@queensu.ca>
2016-10-31 21:08:58 -0400

    Presubmission commit

--- Huffman_1.py
+++ Huffman_1.py
@@ -1,3 +1,12 @@
+"""Part 1 of Assignment 3
+    CMPE 380
+    Nick Lipski
+    Temi Ogunsanya"""
+
+
+""" Node class
+    attributes: characters string, frequency of character's appearance in text file
+    methods: return the character, return the character's frequency, set the left child as a node, set right child as a node """
 class vertex:
     def __init__(self,char,frequency):
         self.char=char
@@ -6,16 +15,20 @@ class vertex:
         self.right=0
         self.ledge=0
         self.redge=1
-    def setLeftchild(self,vi):
+    def setLeftchild(self,vi):  #setting left child
         self.left=vi
-    def setRightchild(self,vj):
+    def setRightchild(self,vj): #setting right child
         self.right=vj
-    def getChar(self):
+    def getChar(self):  #getting character
         return self.char
     def getFreq(self):
         return self.frequency
 
-
+""" dict class
+    used to store generated unique binary and its character
+    attributes: binary, character
+    methods: get binary, get character
+    """
 class dict:
     def __init__(self,char, binary):
         self.char=char
@@ -25,120 +38,86 @@ class dict:
     def getBinary(self):
         return self.binary
 
-
-def Read():
-    chars=[]
-    freq=[]
-    i = 0
-    f = open('data', 'r', encoding='utf-8')
-    char=' '
-    while char:
-        char=f.read(1)
-        if char in chars:
-            freq[chars.index(char)]+=1
+"""Read method
+    reads the data from the file character by character and stores newly identified inside a characters list and its frequency in frequency array
+    Receives a file name that should be opened
+    Returns list of frequencies and list of characters, both in the same order"""
+def Read (filename):
+    chars = []      # initializing  both lists
+    freq = []
+    f = open(filename, 'r', encoding='utf-8') # Opens the file and uses utf-8 encoding
+    char = ' ' # initializes first
+    while char: # loops through each character
+        char = f.read(1)
+        if char in chars:   # checks if the character has already been added to the array
+            freq[chars.index(char)] += 1    # increases the frequency of a character
         else:
-            chars.append(char)
-            freq.append(1)
-    d = open('dictionary', 'r')
-    for line in d:
-        if line[0]!=' ':
-           line.split()
-    chars[4]='$'
-    chars[55]='*'
-    return chars, freq
+            chars.append(char)  # if hasn't been spotted yet, appends to the characters list
+            freq.append(1)  # and sets the frequency to 1
 
+    return chars, freq # returns the lists
 
-def Tree(Chars,Freq):
-    W=Chars.copy()
+"""Tree method
+    Receives the lists with characters and frequencies
+    creates a tree of nodes by combining all the characters into one node based on the frequencies
+    returns a node that branches nad contains a combined string
+    """
+def Tree (Chars,Freq):
+    W=Chars.copy()      #copies frequency and characters lists
     freq=Freq.copy()
-    vertecies=[]
+    vertecies=[]    # creates a list for nodes
     i=0
-    for n in W:
-        vnew= vertex(n,freq[i])
-        vertecies.append(vnew)
+    for n in W:    # loops through characters and frequencies lists
+        vnew= vertex(n,freq[i]) # creates a node for each character and its frequency
+        vertecies.append(vnew)  # appends a node to a new list
         i+=1
 
-    while len(vertecies)>1:
-        ind=freq.index(min(freq))
-        vi=vertecies[ind]
-        vertecies.remove(vi)
+    while len(vertecies)>1: # loops till there is only one parent node
+        ind=freq.index(min(freq))   # finds an index of the node with min frequency
+        vi=vertecies[ind]           # creates it as a node
+        vertecies.remove(vi)        # deletes it from frquencies and characters lists
         freq.remove(freq[ind])
 
-        ind = freq.index(min(freq))
+        ind = freq.index(min(freq)) # same process for the other node
         vj = vertecies[ind]
         vertecies.remove(vj)
         freq.remove(freq[ind])
 
-        Vij=vertex(vi.getChar()+vj.getChar(),vi.getFreq()+vj.getFreq())
-        Vij.setLeftchild(vi)
+        Vij=vertex(vi.getChar()+vj.getChar(),vi.getFreq()+vj.getFreq()) # creates a parent node that combines 2 other nodes
+        Vij.setLeftchild(vi)    # sets those 2 nodes a its children
         Vij.setRightchild(vj)
-        vertecies.append(Vij)
-        freq.append(Vij.getFreq())
+        vertecies.append(Vij)   # appends the parent to a vertices list
+        freq.append(Vij.getFreq())  # appends its frequency to the frequency list for search purposes
     return Vij
 
-def TurnToBinary(V,char):
-    vi = V.left
+
+""" Turning into binary method
+    receives a parent node and a desired character and creates a binary path for the character
+    returns a binary path"""
+def TurnToBinary (V,char):
+    vi = V.left # initialzies children nodes
     vj = V.right
-    if vi != 0 and vj != 0:
-        if char in vi.getChar():
-            return '0'+TurnToBinary(vi,char)
-        elif char in vj.getChar():
-            return '1'+TurnToBinary(vj, char)
+    if vi != 0 or vj != 0:  # checks if its not at the leaf
+        if char in vi.getChar():       # checks if character in the left child
+            return '0'+TurnToBinary(vi,char)    # adds 0 to the path string
+        elif char in vj.getChar():     # checks if character is in the right child
+            return '1'+TurnToBinary(vj, char)   # adds 1 to the path string
     else:
         return ''
 
+"""Main method of the class"""
+Chars,Freq=Read('data') # gets frequency characters lists
+print("Number of unique characters: ", len(Chars))
+V = Tree(Chars, Freq) # gets a parent node
+dictionary=[] # creates a dictionary list
 
-
-
-
-Chars,Freq=Read()
-print(Chars)
-print(Freq)
-print(len(Chars))
-V=Tree(Chars,Freq)
-dictionary=[]
-
-for char in Chars:
+for char in Chars:  # fills up the dictionary list
     bin=dict(char,TurnToBinary(V,char))
     dictionary.append(bin)
 
-string =V.getChar()
-print (string)
-binary=''
-for n in dictionary:
-    print(n.getChar(),"  ",n.getBinary())
-# print("  \n")
-# for n in string:
-#     for m in dictionary:
-#         if n==m.getChar():
-#             binary+=m.getBinary()
-#             break
 Huff_len=0
 i=0
-for char in dictionary:
-    Huff_len+=(len(char.getBinary())*Freq[i])
-    i+=1
-print(Huff_len)
-
-
-
-dictionary_idsd = dictionary.copy()
-print("Dictionary length: ",len(dictionary))
-# for each_bitstring in dictionary:
-#     string=each_bitstring.getBinary()
-#     dictionary_idsd.remove(each_bitstring)
-#     for each_other in dictionary_idsd:
-#         if string in each_other.getBinary():
-#             print("We fucked up")
-
-
-
-
-
-
-
-
-
-
-
-
+for char in dictionary:     # calculates the amount of bits required for newly compressed file
+    Huff_len += (len(char.getBinary())*Freq[i])
+    i += 1
+print("Bits required for encoded file: ", Huff_len)
--- Huffman_2.py
+++ Huffman_2.py
@@ -1,9 +1,20 @@
+"""Part 2 of Assignment 3
+    CMPE 380
+    Nick Lipski
+    Temi Ogunsanya"""
+
+""" dict class
+    used to store generated unique binary and its character
+    attributes: binary, character
+    """
 class dict:
     def __init__(self, char, binary):
         self.char=char
         self.binary=binary
 
-
+"""Read dictionary method
+    reads a a dictionary file
+    returns a list of dictionary list filled with dict objects"""
 def read_dictionary():
     dictionary=[]
     f = open('dictionary', 'r')
@@ -18,42 +29,40 @@ def read_dictionary():
     return dictionary
 
 
+"""Read file method
+    receives a dictionary list
+    returns a decoded string """
 def read_file(dictionary):
-    mystery = ''
+    mystery = ''    # initializing some of the variables
     bin_code = ''
     i = 0
     possible = 0
-    f = open('mystery', 'r')
+    f = open('mystery', 'r')    # reades the encoded file
     char = ' '
     list_of_possible = []
-    while char:
+
+    while char:     # loops each character at a time
         char = f.read(1)
-        i += 1
-        counter=0
+        i += 1  # increments the length counter
+        counter=0   # initializes the counter of matching binaries
         bin_code += char
-        for word in dictionary:
-                if len(word.binary) >= i and word.binary[:i] == bin_code[:i]:
-                    list_of_possible.append(word)
+        for word in dictionary: # loops though the dictionary
+                if len(word.binary) >= i and word.binary[:i] == bin_code[:i]:   # checks if any binary matches the acquired code
+                    list_of_possible.append(word)   # adds to the possibilities list
                     counter+=1
-                if counter>2:
+                if counter>1:   # if there are more than one matching binary then break
                     break
-        possible = len(list_of_possible)
-        if possible == 1:
-            if list_of_possible[0].char=='<LF>':
+        possible = len(list_of_possible)    # number of possibilities
+        if possible == 1:                   # if there is only one possible char
+            if list_of_possible[0].char=='<LF>':    # changes the character
                 list_of_possible[0].char='\n'
-            mystery += list_of_possible[0].char
-            i=0
-            bin_code=''
+            mystery += list_of_possible[0].char # adds the character to the string
+            i = 0   # sets the counter back to zero
+            bin_code = '' # and clears binary string
 
-        list_of_possible = []
-    return mystery
+        list_of_possible = []  # clears up list of possible binaries
 
-dictionary=read_dictionary()
-print(read_file(dictionary))
+    return mystery # returns a decoded string
 
-
-
-
-
-
-
+dictionary=read_dictionary() # creates dictionary list
+print(read_file(dictionary)) # retrieves decoded message
